4a8817763ec24e8f5ac7fbebe68d1f5689185a21
Cathleen Gendron <gendron@mit.edu>
2015-08-08 16:44:06 -0700

    event listeners

--- sumoji.js
+++ sumoji.js
@@ -17,34 +17,10 @@ if (Meteor.isClient) {
   var allPubs = Publishes.find({});
   allPubs.observeChanges({
     added: function(pub) {
-      console.log(pub);
+      // console.log(pub);
     }
   });
 
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    publishes: function() {
-      console.log("method has been called");
-      return Publishes.find({});
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    },
-    'submit .login': function(evt) {
-      evt.preventDefault();
-      var username = evt.target.username.value;
-      var emoji = evt.target.emoji.value;
-      Meteor.subscribe("players", {username: "dlfihsdkfuh"}, function() {
-
-      });
-    }
-  });
 }
 
 if (Meteor.isServer) {
