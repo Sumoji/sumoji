4dc01e084e3efae74dbeb0377e49fc96e686df81
Elliott Marquez <me@elliottmarquez.com>
2015-08-08 20:57:40 -0700

    added javadoc comments

--- server/game.js
+++ server/game.js
@@ -8,16 +8,30 @@ Player: {
 }
 **/
 
+/**
+ * Object representing the game model
+ */
 Game = function() {
   this.maxSpeed = 200;
-
 };
 
+/**
+ * Changes the player object's position property by the velocity vector.
+ * @param  {Player} player [player to be translated]
+ * @return {endefined}
+ */
 Game.prototype.translatePlayer_ = function(player) {
   player.position[0] = player.position[0] + player.velocity.x;
   player.position[1] = player.position[1] + player.velocity.y;
 };
 
+/**
+ * Calculates and applies the velocity of the player caused by a user click.
+ * @param  {String} playerID [Mongo ID of player]
+ * @param  {Number} x        [x click position]
+ * @param  {Number} y        [y click position]
+ * @return {undefined}
+ */
 Game.prototype.applyClick = function(playerID, x, y) {
   // move object with position vector v1 along unit vector v2 by moveSpeed:
   // v1.translate(v2.multiplyByScalar(moveSpeed));
@@ -31,39 +45,69 @@ Game.prototype.applyClick = function(playerID, x, y) {
   var nextVelocity = player.velocity + inputVelocity;
 
   Players.update({_id: player._id}, {$set: {velocity: nextVelocity}});
-}
+};
 
+/**
+ * Shrink's the player mass
+ * @param  {Player} player [player object to shrink]
+ * @return {undefined}
+ */
 Game.prototype.shrink_ = function(player) {
   player.mass = player.mass*.95;
-}
+};
 
+/**
+ * Shrink's the player mass
+ * @param  {Player} player [player object to grow]
+ * @return {undefined}
+ */
 Game.prototype.grow_ = function(player) {
   player.mass = player.mass*1.05;
-}
+};
 
-Game.prototype.inArena_ = function(playerID) {
+/**
+ * Determines whether or not a player is in the arena
+ * @param  {Player} player [Player object in question]
+ * @return {Boolean}       [Whether or not the given player is in the arena]
+ */
+Game.prototype.inArena_ = function(player) {
 
-}
+};
 
+/**
+ * Determines whether or not the player is colliding with the other player
+ * @param  {[type]}  player1 [Player in question]
+ * @param  {[type]}  player2 [Other player in question]
+ * @return {Boolean}         [whether or not the two are colliding]
+ */
 Game.prototype.isColliding_ = function(player1, player2) {
   var p1 = player1.position;
   var p2 = player2.position;
   var r1 = player1.mass / 2;
   var r2 = player2.mass / 2;
 
-  var distanceBtwn = math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2);
-  var collisionDist = math.pow(r1, 2) + math.pow(r2, 2);
+  var distanceBtwn = Math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2);
+  var collisionDist = Math.pow(r1, 2) + math.pow(r2, 2);
 
   return distanceBtwn < distanceBtwn;
-}
+};
 
+/**
+ * Calculates all colliding players in this game
+ * @param  {Array<Player>} players [All players in the game]
+ * @return {Array<Array<Player>>}  [Tuple arrays of colliding players]
+ */
 Game.prototype.collidingPlayers_ = function(players) {
   
-}
+};
 
-Game.prototype.collide_ = function(pID1, pID2) {
-  var player1 = null; // TODO: db fetch
-  var player2 = null; // TODO: db fetch
+/**
+ * Determines the velocity of two colliding players
+ * @param  {Player} player1 [First colliding player.]
+ * @param  {Player} player2 [Second colliding player.]
+ * @return {undefined}
+ */
+Game.prototype.collide_ = function(player1, player2) {
   v1 = player1.velocity;
   v2 = player2.velocity;
   x1 = player1.position;
@@ -77,12 +121,11 @@ Game.prototype.collide_ = function(pID1, pID2) {
   dotProd = (v2 - v1).dot(x2 - x1);
   player2.velocity = v2 - ((2*m1)/(m1 + m2)) *
     (dotProd/normalize(Math.pow(x2 - x1, 2))) * (x2 - x1);
-  return;
-}
+};
 
 Game.prototype.update = function(playerID) {
 
-}
+};
 
 
 var game = new Game();
