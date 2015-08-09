714eefaaed0bf5817efc8bc89e0e1ce6d6fbe570
Steven Anthony Rivera <sarivera@mit.edu>
2015-08-08 18:41:37 -0700

    yay

--- server/game.js
+++ server/game.js
@@ -1,3 +1,30 @@
+/**
+Player: {
+  username: String username
+  emoji: String? which emoji avatar to use
+  mass: Number current size
+  speed: Number current speed
+  position: [Number, Number] current xy-coordinates
+}
+**/
+Player = function(username, emoji, mass, speed, position) {
+  this.username = username;
+  this.emoji = emoji;
+  this.mass = mass;
+  this.speed = speed;
+  // paper.js point
+  this.position = position;
+};
+
+// translateBy is a paper.js vector
+Player.prototype.translate = function(translateBy) {
+  this.position = position + translateBy;
+};
+
+Player.prototype.multiplyByScalar = function(scalar) {
+  this.position *= scalar;
+}
+
 Meteor.methods({
   isInArena: function(player) {
 
@@ -9,7 +36,10 @@ Meteor.methods({
 
   },
   movePlayer: function(player, x, y) {
-
+    // move object with position vector v1 along vector v2 by moveSpeed:
+    // v1.translate(v2.multiplyByScalar(moveSpeed));
+    var speedVect =
+    player.translate()
   },
   isColliding: function(player1, player2) {
 
