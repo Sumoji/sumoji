999686e1d74ac4c912be4146f5cba3868f6934a7
Elliott Marquez <me@elliottmarquez.com>
2015-08-08 21:13:08 -0700

    implemented if in areana

--- server/game.js
+++ server/game.js
@@ -71,7 +71,14 @@ Game.prototype.grow_ = function(player) {
  * @return {Boolean}       [Whether or not the given player is in the arena]
  */
 Game.prototype.inArena_ = function(player) {
+  var x = player.position[0];
+  var y = player.position[1];
 
+  if (x < 0 || y < 0 || x > 1200 || y > 600) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 /**
