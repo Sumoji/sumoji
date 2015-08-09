781620487d0cada51aa107ff4d6ad00cca563223
Elliott Marquez <me@elliottmarquez.com>
2015-08-09 03:45:30 -0700

    bg oscillates

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -4,10 +4,19 @@ Template.connect.helpers({
   }
 });
 
-Meteor.setInterval(function() {
-  $('body').css('background-size', '300px');
-},400);
+var size = 100;
+var diff = 5;
 
 Meteor.setInterval(function() {
-  $('body').css('background-size', '150px');
-},500);
+  $('body').css('background-size', size + 'px');
+  size += diff;
+  if (size == 500) {
+    diff = -5;
+  } else if(size == 5) {
+    diff = 5;
+  }
+},50);
+
+// Meteor.setInterval(function() {
+//   $('body').css('background-size', '150px');
+// },500);
