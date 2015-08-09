d268cf4580983f1afae624ffac077d19276b2dd6
Cathleen Gendron <gendron@mit.edu>
2015-08-08 17:09:01 -0700

    subscribe to playerPositions

---
+++ client/subscriptions.js
@@ -0,0 +1,14 @@
+Meteor.subscribe('playerPositions');
+
+var playersQuery = Players.find({});
+// array - is this reactive?
+var playerObjects = playersQuery.fetch();
+
+playersQuery.observeChanges({
+  added: function(id, fields) {
+    Canvas.update(playerObjects);
+  },
+  changed: function(id, fields) {
+    Canvas.update(playerObjects);
+  }
+});
