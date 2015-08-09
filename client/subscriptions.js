8966a52e906ec5b09a647c66b9810d3ef6548d95
Cathleen Gendron <gendron@mit.edu>
2015-08-08 18:38:51 -0700

    fetch query

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -2,15 +2,16 @@ Meteor.subscribe('playerPositions');
 
 var playersQuery = Players.find({});
 // array - is this reactive?
-var playerObjects = playersQuery.fetch();
 
 playersQuery.observeChanges({
   added: function(id, fields) {
+    var playerObjects = playersQuery.fetch();
     canvas.update(playerObjects);
   },
   changed: function(id, fields) {
     // only re-render if the position has changed
     if (fields.position) {
+      var playerObjects = playersQuery.fetch();
       canvas.update(playerObjects);
     }
   }
